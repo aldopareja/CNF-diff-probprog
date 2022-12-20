d32c15fd4861e0109e6fa8d373af124d058ef417
aldo <7622817+aldopareja@users.noreply.github.com>
2022-12-20 10:57:03 -0600

    format with black

--- cnf.py
+++ cnf.py
@@ -1,4 +1,4 @@
-#Credit, inspired by https://docs.kidger.site/diffrax/examples/continuous_normalising_flow/
+# Credit, inspired by https://docs.kidger.site/diffrax/examples/continuous_normalising_flow/
 
 from typing import List
 
@@ -7,6 +7,7 @@ from jax import numpy as jnp
 from jax.random import split
 
 from tensorflow_probability.substrates import jax as tfp
+
 tfd = tfp.distributions
 
 import equinox as eqx
@@ -14,55 +15,58 @@ import diffrax
 
 from utils import augment_sample
 
+
 class ConcatSquash(eqx.Module):
-  lin1: eqx.nn.Linear
-  lin2: eqx.nn.Linear
-  lin3: eqx.nn.Linear
-  
-  def __init__(self, *, in_size, out_size, key, **kwargs):
-    super().__init__(*kwargs)
-    k1, k2, k3 = split(key, 3)
-    self.lin1 = eqx.nn.Linear(in_size, out_size, key=k1)
-    self.lin2 = eqx.nn.Linear(1, out_size, key=k2)
-    self.lin3 = eqx.nn.Linear(1, out_size, use_bias=False, key=k3)
-    
-  def __call__(self, t, x):
-    return self.lin1(x) * jax.nn.sigmoid(self.lin2(t)) + self.lin3(t)
+    lin1: eqx.nn.Linear
+    lin2: eqx.nn.Linear
+    lin3: eqx.nn.Linear
+
+    def __init__(self, *, in_size, out_size, key, **kwargs):
+        super().__init__(*kwargs)
+        k1, k2, k3 = split(key, 3)
+        self.lin1 = eqx.nn.Linear(in_size, out_size, key=k1)
+        self.lin2 = eqx.nn.Linear(1, out_size, key=k2)
+        self.lin3 = eqx.nn.Linear(1, out_size, use_bias=False, key=k3)
+
+    def __call__(self, t, x):
+        return self.lin1(x) * jax.nn.sigmoid(self.lin2(t)) + self.lin3(t)
+
 
 class Func(eqx.Module):
-  layers: List[eqx.nn.Linear]
-  
-  def __init__(self, *, num_latents, num_conds, width_size, depth, key, **kwargs):
-    super().__init__(**kwargs)
-    assert depth>=1
-    ks = split(key,depth+1)
-    layers = []
-    layers.append(
-      ConcatSquash(in_size=num_latents+num_conds, out_size=width_size, key=ks[0])
-    )
-    for i in range(depth - 1):
-      layers.append(
-        ConcatSquash(
-          in_size=width_size, out_size=width_size, key=ks[i+1]
+    layers: List[eqx.nn.Linear]
+
+    def __init__(self, *, num_latents, num_conds, width_size, depth, key, **kwargs):
+        super().__init__(**kwargs)
+        assert depth >= 1
+        ks = split(key, depth + 1)
+        layers = []
+        layers.append(
+            ConcatSquash(
+                in_size=num_latents + num_conds, out_size=width_size, key=ks[0]
+            )
         )
-      )
-    layers.append(
-      ConcatSquash(in_size=width_size, out_size=num_latents, key=ks[-1])
-    )
-    self.layers = layers
-  
-  def __call__(self, t, z, args):
-    cond_vars = args[0]
-    assert len(cond_vars.shape) == 1
-    t = jnp.asarray(t)[None]
-    z = self.layers[0](t,jnp.concatenate([z,cond_vars]))
-    z = jax.nn.tanh(z)
-    for layer in self.layers[1:-1]:
-      z = layer(t,z)
-      z = jax.nn.tanh(z)
-    dz_dt = self.layers[-1](t,z)
-    return dz_dt
-  
+        for i in range(depth - 1):
+            layers.append(
+                ConcatSquash(in_size=width_size, out_size=width_size, key=ks[i + 1])
+            )
+        layers.append(
+            ConcatSquash(in_size=width_size, out_size=num_latents, key=ks[-1])
+        )
+        self.layers = layers
+
+    def __call__(self, t, z, args):
+        cond_vars = args[0]
+        assert len(cond_vars.shape) == 1
+        t = jnp.asarray(t)[None]
+        z = self.layers[0](t, jnp.concatenate([z, cond_vars]))
+        z = jax.nn.tanh(z)
+        for layer in self.layers[1:-1]:
+            z = layer(t, z)
+            z = jax.nn.tanh(z)
+        dz_dt = self.layers[-1](t, z)
+        return dz_dt
+
+
 def exact_logp_wrapper(t, z, args):
     z, _ = z
     *args, func = args
@@ -74,63 +78,67 @@ def exact_logp_wrapper(t, z, args):
     logp = jnp.trace(dfdy)
     return f, logp
 
+
 class CNF(eqx.Module):
-  funcs: List[Func]
-  num_latents: int
-  num_augments: int
-  t0: float
-  t1: float
-  dt0: float
-  
-  def __init__(
-    self,
-    *,
-    num_latents,
-    num_augments = 0,
-    num_conds,
-    width_size,
-    num_blocks,
-    depth,
-    key,
-    **kwargs,  
-  ):
-    super().__init__(**kwargs)
-    ks = split(key, num_blocks)
-    self.funcs = [
-      Func(
-        num_latents=num_latents + num_augments,
-        num_conds=num_conds,
-        width_size=width_size,
-        depth=depth,
-        key=k,
-      )
-      for k in ks
-    ]
-    self.num_latents = num_latents
-    self.num_augments = num_augments
-    self.t0 = 0
-    self.t1 = 1
-    self.dt0 = 0.1
-  
-  def log_p(self, z, cond_vars, key):
-    z_aug = augment_sample(key,z,self.num_augments)
-    term = diffrax.ODETerm(exact_logp_wrapper)
-    solver = diffrax.Tsit5(scan_stages=False)
-    delta_log_likelihood = 0.0
-    for func in reversed(self.funcs):
-      z_aug = (z_aug, delta_log_likelihood)
-      sol = diffrax.diffeqsolve(
-        term, solver, self.t1, self.t0, -self.dt0, z_aug, (cond_vars, func)
-      )
-      (z_aug,), (delta_log_likelihood,) = sol.ys
-    return delta_log_likelihood + tfd.Normal(0,1).log_prob(z_aug).sum()
-  
-  def rsample(self, key, cond_vars):
-    z = tfd.Normal(0,1).sample(seed=key, sample_shape=(self.num_latents + self.num_augments,))
-    for func in self.funcs:
-      term = diffrax.ODETerm(func)
-      solver = diffrax.Tsit5()
-      sol = diffrax.diffeqsolve(term, solver, self.t0, self.t1, self.dt0, z, (cond_vars,))
-      (z,) = sol.ys
-    return z[:self.num_latents]
-  
+    funcs: List[Func]
+    num_latents: int
+    num_augments: int
+    t0: float
+    t1: float
+    dt0: float
+
+    def __init__(
+        self,
+        *,
+        num_latents,
+        num_augments=0,
+        num_conds,
+        width_size,
+        num_blocks,
+        depth,
+        key,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        ks = split(key, num_blocks)
+        self.funcs = [
+            Func(
+                num_latents=num_latents + num_augments,
+                num_conds=num_conds,
+                width_size=width_size,
+                depth=depth,
+                key=k,
+            )
+            for k in ks
+        ]
+        self.num_latents = num_latents
+        self.num_augments = num_augments
+        self.t0 = 0
+        self.t1 = 1
+        self.dt0 = 0.1
+
+    def log_p(self, z, cond_vars, key):
+        z_aug = augment_sample(key, z, self.num_augments)
+        term = diffrax.ODETerm(exact_logp_wrapper)
+        solver = diffrax.Tsit5(scan_stages=False)
+        delta_log_likelihood = 0.0
+        for func in reversed(self.funcs):
+            z_aug = (z_aug, delta_log_likelihood)
+            sol = diffrax.diffeqsolve(
+                term, solver, self.t1, self.t0, -self.dt0, z_aug, (cond_vars, func)
+            )
+            (z_aug,), (delta_log_likelihood,) = sol.ys
+        return delta_log_likelihood + tfd.Normal(0, 1).log_prob(z_aug).sum()
+
+    def rsample(self, key, cond_vars):
+        z = tfd.Normal(0, 1).sample(
+            seed=key, sample_shape=(self.num_latents + self.num_augments,)
+        )
+        for func in self.funcs:
+            term = diffrax.ODETerm(func)
+            solver = diffrax.Tsit5()
+            sol = diffrax.diffeqsolve(
+                term, solver, self.t0, self.t1, self.dt0, z, (cond_vars,)
+            )
+            (z,) = sol.ys
+        return z[: self.num_latents]
--- test_cnf.py
+++ test_cnf.py
@@ -6,89 +6,101 @@ import equinox as eqx
 from cnf import CNF
 import time
 
+
 def test_CNF():
-  '''
-  test if the conditional CNF can learn to produce 
-  a normal with mean 0.5,-0.5 depending on a bernoulli
-  '''
-  model = CNF(
-    num_latents=1,
-    num_augments=9,
-    num_conds=1,
-    width_size=128,
-    num_blocks=1,
-    depth=3,
-    key=PRNGKey(0)
-  )
-  
-  def sample(k):
-    '''sample a bernoulli and then sample the normal based on the value of the bernoulli.
-    Augment the flow with standard normals to make it easier to learn.
-    '''
-    ks = split(k,3)
-    b = bernoulli(ks[0],0.5)
-    s = normal(ks[1])
-    s /= 10.0
-    s += jax.lax.cond(b, lambda: 0.5, lambda: -.5)
-    # s = jnp.concatenate([s[None], normal(ks[2],shape=(9,))])
-    return s[None], b[None]
-    # return s[None], b[None]
-  
-  num_steps = 200
-  optim = optax.chain(
+    """
+    test if the conditional CNF can learn to produce
+    a normal with mean 0.5,-0.5 depending on a bernoulli
+    """
+    model = CNF(
+        num_latents=1,
+        num_augments=9,
+        num_conds=1,
+        width_size=128,
+        num_blocks=1,
+        depth=3,
+        key=PRNGKey(0),
+    )
+
+    def sample(k):
+        """sample a bernoulli and then sample the normal based on the value of the bernoulli.
+        Augment the flow with standard normals to make it easier to learn.
+        """
+        ks = split(k, 3)
+        b = bernoulli(ks[0], 0.5)
+        s = normal(ks[1])
+        s /= 10.0
+        s += jax.lax.cond(b, lambda: 0.5, lambda: -0.5)
+        # s = jnp.concatenate([s[None], normal(ks[2],shape=(9,))])
+        return s[None], b[None]
+        # return s[None], b[None]
+
+    num_steps = 200
+    optim = optax.chain(
         optax.clip_by_global_norm(5.0),
-        optax.adamw(learning_rate=optax.cosine_onecycle_schedule(
-                                      num_steps,
-                                      0.025,
-                                      0.01,
-                                      1e0,
-                                      1e2,
-                                  ), 
-                    weight_decay=0.0005),
+        optax.adamw(
+            learning_rate=optax.cosine_onecycle_schedule(
+                num_steps,
+                0.025,
+                0.01,
+                1e0,
+                1e2,
+            ),
+            weight_decay=0.0005,
+        ),
     )
-  batch_size = 1000
-  opt_state = optim.init(eqx.filter(model,eqx.is_inexact_array))
-  
-  @eqx.filter_value_and_grad
-  def loss(model, s_batch, b_batch, key):
-    log_p = jax.vmap(model.log_p)(s_batch,b_batch, split(key, b_batch.shape[0]))
-    return -jnp.mean(log_p)
-  
-  @eqx.filter_jit
-  def make_step(model, opt_state, key):
-    ks = split(key, batch_size+2)
-    s_batch, b_batch = jax.vmap(sample)(ks[:-1])
-    l, grads = loss(model, s_batch, b_batch, ks[-2])
-    updates, opt_state = optim.update(grads, opt_state, model)
-    model = eqx.apply_updates(model, updates)
-    return l, model, opt_state, ks[-1]
-  
-  key = PRNGKey(573)
-  for i in range(num_steps):
-    start = time.time()
-    l, model, opt_state, key = make_step(
-      model, opt_state, key
+    batch_size = 1000
+    opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
+
+    @eqx.filter_value_and_grad
+    def loss(model, s_batch, b_batch, key):
+        log_p = jax.vmap(model.log_p)(s_batch, b_batch, split(key, b_batch.shape[0]))
+        return -jnp.mean(log_p)
+
+    @eqx.filter_jit
+    def make_step(model, opt_state, key):
+        ks = split(key, batch_size + 2)
+        s_batch, b_batch = jax.vmap(sample)(ks[:-1])
+        l, grads = loss(model, s_batch, b_batch, ks[-2])
+        updates, opt_state = optim.update(grads, opt_state, model)
+        model = eqx.apply_updates(model, updates)
+        return l, model, opt_state, ks[-1]
+
+    key = PRNGKey(573)
+    for i in range(num_steps):
+        start = time.time()
+        l, model, opt_state, key = make_step(model, opt_state, key)
+        end = time.time()
+        if i % 20 == 0 or i == 1:
+            print("l", l, "t", end - start)
+        if i % 100 == 0:
+            pos_sample = jax.vmap(model.rsample)(split(key, 1000), jnp.ones((1000, 1)))
+            print(
+                "pos_sample_mean",
+                pos_sample[:, 0].mean(),
+                pos_sample[:, 0].std(),
+                pos_sample.shape,
+            )
+
+            neg_sample = jax.vmap(model.rsample)(split(key, 1000), jnp.zeros((1000, 1)))
+            print("neg_sample_mean", neg_sample[:, 0].mean(), neg_sample[:, 0].std())
+
+    pos_sample = jax.vmap(model.rsample)(split(key, 1000), jnp.ones((1000, 1)))
+    print(
+        "pos_sample_mean",
+        pos_sample[:, 0].mean(),
+        pos_sample[:, 0].std(),
+        pos_sample.shape,
     )
-    end = time.time()
-    if i % 20 == 0 or i == 1:
-      print('l',l,'t', end-start)
-    if i % 100 == 0:
-      pos_sample = jax.vmap(model.rsample)(split(key,1000), jnp.ones((1000,1)))
-      print('pos_sample_mean', pos_sample[:,0].mean(), pos_sample[:,0].std(), pos_sample.shape)
-      
-      neg_sample = jax.vmap(model.rsample)(split(key,1000), jnp.zeros((1000,1)))
-      print('neg_sample_mean', neg_sample[:,0].mean(), neg_sample[:,0].std())
-  
-  pos_sample = jax.vmap(model.rsample)(split(key,1000), jnp.ones((1000,1)))
-  print('pos_sample_mean', pos_sample[:,0].mean(), pos_sample[:,0].std(), pos_sample.shape)
-  
-  neg_sample = jax.vmap(model.rsample)(split(key,1000), jnp.zeros((1000,1)))
-  print('neg_sample_mean', neg_sample[:,0].mean(), neg_sample[:,0].std())
-  
-  assert jnp.isclose(pos_sample[:,0].mean(), 0.5, rtol=0.1)
-  assert jnp.isclose(neg_sample[:,0].mean(), -0.5, rtol=0.1)
-  assert jnp.isclose(pos_sample[:,0].std(), 0.1, rtol=0.1)
-  assert jnp.isclose(neg_sample[:,0].std(), 0.1, rtol=0.1)
-  
+
+    neg_sample = jax.vmap(model.rsample)(split(key, 1000), jnp.zeros((1000, 1)))
+    print("neg_sample_mean", neg_sample[:, 0].mean(), neg_sample[:, 0].std())
+
+    assert jnp.isclose(pos_sample[:, 0].mean(), 0.5, rtol=0.1)
+    assert jnp.isclose(neg_sample[:, 0].mean(), -0.5, rtol=0.1)
+    assert jnp.isclose(pos_sample[:, 0].std(), 0.1, rtol=0.1)
+    assert jnp.isclose(neg_sample[:, 0].std(), 0.1, rtol=0.1)
+
+
 if __name__ == "__main__":
-  test_CNF()
+    test_CNF()
