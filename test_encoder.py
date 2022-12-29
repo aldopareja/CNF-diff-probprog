52f97a11abb11faa760c6cf3d952c640e5760bc6
aldo <7622817+aldopareja@users.noreply.github.com>
2022-12-29 11:23:08 -0600

    trained number of mixtures with known means

--- test_encoder.py
+++ test_encoder.py
@@ -25,14 +25,14 @@ class EncoderClassifier(eqx.Module):
       num_heads=4,
       dropout_rate=0.1,
       d_model=52,
-      num_input_variables=2,
-      num_enc_layers=2
+      num_input_variables=1,
+      num_enc_layers=3
       ),
     )
     self.mlp = eqx.nn.MLP(52,6,width_size=52,depth=1,key=ks[1])
     
   def __call__(self,x, key):
-    assert x.ndim == 2 and x.shape[1] ==2
+    assert x.ndim == 2 and x.shape[1] ==1
     enc_obs = self.encode(x,key=key)
     return self.mlp(enc_obs)
 
@@ -45,20 +45,20 @@ def test_encoder():
   m = EncoderClassifier(key=PRNGKey(5543))
   
   obs_size = 100
-  def sample(k,*, max_num_mixtures=6, dims=2):
+  def sample(k,*, max_num_mixtures=6, dims=1):
     """sample a bernoulli and then sample the normal based on the value of the bernoulli.
     Augment the flow with standard normals to make it easier to learn.
     """
     ks = split(k, 4)
-    # b = bernoulli(ks[0], 0.5)
+    
     b = tfd.Categorical(
         probs=jnp.ones((max_num_mixtures,)) / max_num_mixtures
     ).sample(seed=ks[0])
     
     class_labels_probs = jnp.where(
-    jnp.arange(max_num_mixtures) < b,
-    jnp.ones((max_num_mixtures,)),
-    jnp.zeros((max_num_mixtures,)),
+      jnp.arange(max_num_mixtures) <= b,
+      jnp.ones((max_num_mixtures,)),
+      jnp.zeros((max_num_mixtures,)),
     )
 
     class_labels = tfd.Categorical(probs=class_labels_probs).sample(
@@ -67,52 +67,18 @@ def test_encoder():
     
     s = normal(ks[1],shape=(obs_size,dims))
     s /= 10.0
-    # means = (jnp.stack([jnp.arange(max_num_mixtures)]*dims,axis=1)/max_num_mixtures)
-    # s += means[b].reshape(-1,dims)
-    # mean = (jnp.arange(max_num_mixtures)[b]/max_num_mixtures).reshape(-1,1)
-    mean = (jnp.stack([jnp.arange(max_num_mixtures)]*dims,axis=1)/max_num_mixtures)[b].reshape(-1,dims)
+    
+    mean = (jnp.stack([jnp.arange(max_num_mixtures)]*dims,axis=1)/max_num_mixtures)[class_labels].reshape(-1,dims)
     s += mean
     return s, jnp.int32(b)
-  
-  # def sample_obs(means, class_label, k):
-  #   return tfd.MultivariateNormalDiag(
-  #     loc = means[class_label],
-  #     scale_diag=jnp.ones_like(means[class_label])/10.0
-  #   ).sample(seed=k)
-  
-  # def sample(k, *, max_num_mixtures=6, dims=2, num_obs=100):
-  #   ks = split(k, 5)
-  #   num_mixtures = tfd.Categorical(
-  #       probs=jnp.ones((max_num_mixtures,)) / max_num_mixtures
-  #   ).sample(seed=ks[0])
-    
-  #   means = tfd.Uniform(low=-1.0, high=1.0).sample(
-  #       seed=ks[1], sample_shape=(max_num_mixtures, dims)
-  #   )
-    
-  #   class_labels_probs = jnp.where(
-  #       jnp.arange(max_num_mixtures) < num_mixtures,
-  #       jnp.ones((max_num_mixtures,)),
-  #       jnp.zeros((max_num_mixtures,)),
-  #   )
-    
-  #   class_labels = tfd.Categorical(probs=class_labels_probs).sample(
-  #       seed=ks[3], sample_shape=(num_obs,)
-  #   )
-    
-  #   obs = jax.vmap(sample_obs, in_axes=(None,0,0))(
-  #     means,class_labels,split(ks[4],num_obs)
-  #   )
-    
-  #   return obs, num_mixtures
 
-  num_steps = 1000
+  num_steps = 10000
   optim = optax.chain(
      optax.clip_by_global_norm(5.0),
         optax.adamw(
             learning_rate=optax.cosine_onecycle_schedule(
                 num_steps,
-                0.025,
+                0.002,
                 0.01,
                 1e0,
                 1e2,
@@ -126,7 +92,7 @@ def test_encoder():
   
   @eqx.filter_value_and_grad
   def loss(m, obs, b, key):
-    assert obs.ndim == 3 and obs.shape[2]==2 and jnp.dtype(b) == jnp.int32 and b.ndim == 1
+    assert obs.ndim == 3 and obs.shape[2]==1 and jnp.dtype(b) == jnp.int32 and b.ndim == 1
     logits = jax.vmap(m)(obs,split(key,obs.shape[0]))
     @jax.vmap
     def log_p(logits,b):
