dfae35497f1d6a4b2161aaf423d7bc0d51c52235
aldo <7622817+aldopareja@users.noreply.github.com>
2023-01-22 19:05:33 -0600

    coded up real_nvp as a faster discrete flow

---
+++ real_nvp.py
@@ -0,0 +1,166 @@
+from typing import List
+
+import jax
+from jax import numpy as jnp
+from jax.random import PRNGKey, split
+from jaxtyping import Array
+
+from tensorflow_probability.substrates import jax as tfp
+tfd = tfp.distributions
+
+import equinox as eqx
+
+from utils import augment_sample
+
+
+def concat_elu(x):
+  assert x.ndim == 1
+  return jnp.concatenate([jax.nn.elu(x), jax.nn.elu(-x)])
+
+class GatedDenseLayer(eqx.Module):
+  l1: eqx.nn.Linear
+  l2: eqx.nn.Linear
+  
+  def __init__(self, *, hidden_size, key:PRNGKey):
+    super().__init__()
+    k1, k2 = split(key)
+    self.l1 = eqx.nn.Linear(hidden_size*2,hidden_size,key=k1)
+    self.l2 = eqx.nn.Linear(hidden_size*2, hidden_size*2,key=k2)
+    
+  def __call__(self, x):
+    assert x.ndim == 1
+    out = concat_elu(x)
+    out = self.l1(out)
+    out = concat_elu(out)
+    out = self.l2(out)
+    val, gate = out.split(2)
+    return x + val * jax.nn.sigmoid(gate)
+  
+class GatedDenseNet(eqx.Module):
+  l_in: eqx.nn.Linear
+  layers: List[GatedDenseLayer]
+  l_out: eqx.nn.Linear
+  in_size: int
+  
+  def __init__(self, *, num_layers, in_size, out_size ,hidden_size, key):
+    ks = split(key,num_layers + 2)
+    self.l_in = eqx.nn.Linear(in_size, hidden_size, key=ks[-1])
+    self.layers = [GatedDenseLayer(hidden_size=hidden_size, key=ks[i]) for i in range(num_layers)]
+    self.l_out = eqx.nn.Linear(hidden_size*2, out_size, key=ks[-2])
+    self.in_size = in_size
+    
+  def __call__(self, x):
+    assert x.ndim == 1 and x.shape[0] == self.in_size
+    x = self.l_in(x)
+    for l in self.layers:
+      x = l(x)
+    x = concat_elu(x)
+    x = self.l_out(x)
+    
+    return x
+  
+class RealNVPLayer(eqx.Module):
+  # mask: Array
+  # non_mask: Array
+  num_mask: int
+  nn: GatedDenseNet
+  scaling_factor: Array
+  
+  def __init__(self,*,num_layers,num_variables, hidden_size, num_conds, key):
+    assert num_variables % 2 == 0 #require odd num_variables to properly split
+    # mask = jnp.arange(num_variables)
+    # self.mask = mask[:num_variables//2] if even else mask[num_variables//2:]
+    # self.non_mask = mask[:num_variables//2] if not even else mask[num_variables//2:]
+    self.nn = GatedDenseNet(num_layers=num_layers,
+                            in_size=num_variables//2 + num_conds,
+                            out_size=num_variables,
+                            hidden_size=hidden_size,
+                            key=key)
+    self.scaling_factor = jnp.zeros(num_variables//2)
+    self.num_mask = num_variables//2
+    
+  def forward(self, x, cond_vars):
+    '''
+    takes the first half to compute the scale and shift and applies it
+    to the second half. Returns the same first half but a different second half.
+    We permute the order so the next layer transforms the first half.
+    '''
+    assert x.ndim == 1
+    x_0 = x[:self.num_mask]
+    x_1 = x[self.num_mask:]
+    
+    in_x = jnp.concatenate([x_0,cond_vars])
+    log_scale, shift = self.nn(in_x).split(2)
+    
+    #stabilize the flow
+    s_fac = jnp.exp(self.scaling_factor)
+    log_scale = jax.nn.tanh(log_scale/s_fac) * log_scale
+    
+    y_1 = x_1 * jnp.exp(log_scale) + shift
+    
+    log_det_jac = log_scale.sum()
+    
+    return jnp.concatenate([y_1, x_0]), log_det_jac
+  
+  def inverse(self, y, cond_vars):
+    '''
+    takes the second half to compute the same scale and shift computed in the forward
+    uses this value to unscale and unshift the first half and returns the same
+    values that were fed in the beginning.
+    '''
+    assert y.ndim == 1
+    
+    x_0 = y[self.num_mask:]
+    y_1 = y[:self.num_mask]
+    
+    in_x = jnp.concatenate([x_0,cond_vars])
+    log_scale, shift = self.nn(in_x).split(2)
+    
+    #stabilize the flow
+    s_fac = jnp.exp(self.scaling_factor)
+    log_scale = jax.nn.tanh(log_scale/s_fac) * log_scale
+    
+    x_1 = (y_1 - shift) * jnp.exp(-log_scale)
+    
+    log_det_jac = -log_scale.sum()
+    
+    return jnp.concatenate([x_0, x_1]), log_det_jac
+    
+class RealNVP_Flow(eqx.Module):
+  blocks: List[RealNVPLayer]
+  num_latents: int
+  num_augments: int
+  
+  def __init__(self,*,num_blocks, num_layers_per_block, block_hidden_size, num_augments, num_latents, num_conds, key):
+    ks = split(key, num_blocks)
+    self.blocks = [RealNVPLayer(num_layers=num_layers_per_block,
+                                num_variables=num_latents+num_augments,
+                                num_conds=num_conds,
+                                hidden_size=block_hidden_size,
+                                key=ks[i])
+                   for i in range(num_blocks)]
+    self.num_latents = num_latents
+    self.num_augments = num_augments
+    
+  def log_p(self, z, cond_vars, key):
+    assert z.ndim == 1 and z.shape[0] == self.num_latents
+    z_aug = augment_sample(key, z, self.num_augments)
+    log_prob = 0
+    
+    for block in reversed(self.blocks):
+      z_aug, inv_log_det_jac = block.inverse(z_aug,cond_vars)
+      log_prob += inv_log_det_jac
+      
+    log_prob += tfd.Normal(0, 1).log_prob(z_aug).sum() 
+    
+    return log_prob
+  
+  def rsample(self, key, cond_vars):
+    z = tfd.Normal(0, 1).sample(
+      seed = key, sample_shape=(self.num_latents + self.num_augments,)
+    )
+    
+    for block in self.blocks:
+      z, _ = block.forward(z, cond_vars)
+      
+    return z[:self.num_latents]
---
+++ test_real_nvp.py
@@ -0,0 +1,120 @@
+import optax
+import jax
+from jax.random import PRNGKey, split, bernoulli, normal
+from jax import numpy as jnp
+import equinox as eqx
+
+from real_nvp import RealNVPLayer, RealNVP_Flow
+
+import time
+
+def test_real_nvp():
+  '''
+  start testing that RealNVPLayer is invertible
+  '''
+  nvp = RealNVPLayer(
+    num_layers= 1, num_variables= 10, hidden_size=100, num_conds=10, key=PRNGKey(0)
+  )
+  
+  x = jax.random.uniform(key=PRNGKey(0), shape=(10,))
+  
+  x_hat,_ = nvp.forward(x,x)
+  x_hat,_ = nvp.inverse(x_hat,x)
+  
+  assert jnp.allclose(x,x_hat)
+  
+  '''
+  test if a complete flow can predict the mean of a gaussian based on a bernoulli
+  '''
+  
+  model = RealNVP_Flow(
+    num_blocks = 6,
+    num_layers_per_block=1,
+    block_hidden_size=64,
+    num_augments=29,
+    num_latents=1,
+    num_conds=1,
+    key=PRNGKey(0)
+  )
+  
+  def sample(k):
+    """sample a bernoulli and then sample the normal based on the value of the bernoulli.
+    Augment the flow with standard normals to make it easier to learn.
+    """
+    ks = split(k, 3)
+    b = bernoulli(ks[0], 0.5)
+    s = normal(ks[1])
+    s /= 10.0
+    s += jax.lax.cond(b, lambda: 0.5, lambda: -0.5)
+    # s = jnp.concatenate([s[None], normal(ks[2],shape=(9,))])
+    return s[None], b[None]
+    # return s[None], b[None]
+
+  num_steps = 10000
+  optim = optax.chain(
+      optax.clip_by_global_norm(5.0),
+      optax.adamw(
+          learning_rate=optax.cosine_onecycle_schedule(
+              num_steps,
+              0.015,
+              0.01,
+              1e0,
+              1e1,
+          ),
+          weight_decay=0.0005,
+      ),
+  )
+  batch_size = 10000
+  opt_state = optim.init(eqx.filter(model, eqx.is_inexact_array))
+
+  @eqx.filter_value_and_grad
+  def loss(model, s_batch, b_batch, key):
+      log_p = jax.vmap(model.log_p)(s_batch, b_batch, split(key, b_batch.shape[0]))
+      return -jnp.mean(log_p)
+
+  @eqx.filter_jit
+  def make_step(model, opt_state, key):
+      ks = split(key, batch_size + 2)
+      s_batch, b_batch = jax.vmap(sample)(ks[:-2])
+      l, grads = loss(model, s_batch, b_batch, ks[-2])
+      updates, opt_state = optim.update(grads, opt_state, model)
+      model = eqx.apply_updates(model, updates)
+      return l, model, opt_state, ks[-1]
+
+  key = PRNGKey(573)
+  for i in range(num_steps):
+      start = time.time()
+      l, model, opt_state, key = make_step(model, opt_state, key)
+      end = time.time()
+      if i % 100 == 0 or i == 1:
+          print("l", l, "t", end - start)
+      if i % 200 == 0:
+          pos_sample = jax.vmap(model.rsample)(split(key, 10000), jnp.ones((10000, 1)))
+          print(
+              "pos_sample_mean",
+              pos_sample[:, 0].mean(),
+              pos_sample[:, 0].std(),
+              pos_sample.shape,
+          )
+
+          neg_sample = jax.vmap(model.rsample)(split(key, 10000), jnp.zeros((10000, 1)))
+          print("neg_sample_mean", neg_sample[:, 0].mean(), neg_sample[:, 0].std())
+
+  pos_sample = jax.vmap(model.rsample)(split(key, 10000), jnp.ones((10000, 1)))
+  print(
+      "pos_sample_mean",
+      pos_sample[:, 0].mean(),
+      pos_sample[:, 0].std(),
+      pos_sample.shape,
+  )
+
+  neg_sample = jax.vmap(model.rsample)(split(key, 10000), jnp.zeros((10000, 1)))
+  print("neg_sample_mean", neg_sample[:, 0].mean(), neg_sample[:, 0].std())
+
+  assert jnp.isclose(pos_sample[:, 0].mean(), 0.5, rtol=0.1)
+  assert jnp.isclose(neg_sample[:, 0].mean(), -0.5, rtol=0.1)
+  assert jnp.isclose(pos_sample[:, 0].std(), 0.1, rtol=0.1)
+  assert jnp.isclose(neg_sample[:, 0].std(), 0.1, rtol=0.1)
+  
+if __name__ == "__main__":
+  test_real_nvp()
