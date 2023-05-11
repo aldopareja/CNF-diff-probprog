37646f93ef5ae9e81d74b94f432c04b7329fb4ba
aldo <7622817+aldopareja@users.noreply.github.com>
2023-05-11 00:56:33 -0500

    files to train bayesian regression

---
+++ experiments/bayesian_reg/bayesian_regression.py
@@ -0,0 +1,83 @@
+import numpyro as npy
+from numpyro import distributions as dist
+from jax.random import PRNGKey, split
+from numpyro.handlers import trace
+from jax import numpy as jnp
+from src.utils.trace_dataset import sample_many_traces, serialize_traces
+import numpy as np
+from scipy.stats import multivariate_normal, norm
+
+def bayesian_linear_regression(key, lk_sigma, prior_sigma, x):
+  ks = split(key, 3)
+  x1 = npy.sample("x1", dist.Normal(0, prior_sigma), rng_key=ks[0])
+  x2 = npy.sample("x2", dist.Normal(0, prior_sigma), rng_key=ks[1])
+  
+  x_ = x1 * x + x2
+  
+  obs = dist.Normal(x_, lk_sigma).sample(key=ks[2])[:,None]
+  
+  npy.deterministic("obs", obs)
+  
+  return obs
+
+def analytical_covariance(x, sigma, alpha):
+    cov1 = np.identity(2)*(sigma/alpha)**2
+    x0_sum = len(x)
+    x1_sum = np.sum(x)
+    x2_sum = np.sum(x**2)
+    cov2 = np.array([
+        [x0_sum, x1_sum],
+        [x1_sum, x2_sum],
+    ])
+    cov = (1./sigma**2)*(cov1+cov2)
+    cov = np.linalg.inv(cov)
+    return cov
+
+# Analytical expression for the posterior mean
+def analytical_mean(x, y, sigma, alpha):
+    xy_sum = np.sum(x*y)
+    y_sum = np.sum(y)
+    vec = np.array([y_sum, xy_sum])
+    cov = analytical_covariance(x, sigma, alpha)
+    mu = np.matmul(cov, vec)/sigma**2
+    return mu
+  
+# Distribution for 1D marginal posteriors
+def get_distribution(plt, mu, sig, N=200):
+    xlim = plt.gca().get_xlim()
+    x = np.linspace(xlim[0], xlim[1], N)
+    rv = norm(mu, sig)
+    f = rv.pdf(x)
+    return x, f
+
+# Contours for 2D posterior distributions
+def get_contours(plt, mu, cov, N=200):
+    xlim = plt.gca().get_xlim(); ylim = plt.gca().get_ylim()
+    X = np.linspace(xlim[0], xlim[1], N)
+    Y = np.linspace(ylim[0], ylim[1], N)
+    X, Y = np.meshgrid(X, Y); pos = np.dstack((X, Y))
+    rv = multivariate_normal(mu, cov)
+    Z = rv.pdf(pos)
+    return X, Y, Z
+
+def plot_posterior(plt, mu, cov, proposal_samples,):
+    plt.scatter([sample['x2'] for sample in proposal_samples], [sample['x1'] for sample in proposal_samples], alpha=0.5, s=5., label="proposal", color='red')
+    if mu is not None and cov is not None:
+        X, Y, Z = get_contours(plt, mu, cov)
+        plt.contourf(X, Y, Z, alpha=0.5, cmap='viridis')
+        plt.colorbar(label="Posterior density")
+    plt.xlabel('bias')
+    plt.ylabel('slope')
+  
+  
+
+if __name__ == "__main__":
+  tr = trace(bayesian_linear_regression).get_trace(PRNGKey(0), 0.1, 10.0, jnp.arange(6.0)+1)
+
+  sampler_ = lambda key: bayesian_linear_regression(key, 0.1, 10.0, jnp.arange(6.0)+1)
+  traces, default_trace, means_and_stds = sample_many_traces(sampler_, PRNGKey(0), 100000, True, max_num_variables=100, max_traces_for_default_trace=10000)
+
+  serialize_traces(traces, "experiments/bayesian_linear_regression/traces.pkl")
+  serialize_traces(means_and_stds, "experiments/bayesian_linear_regression/means_and_stds.pkl")
+    
+  from IPython import embed; embed(using=False)
---
+++ experiments/bayesian_reg/train_bayesian_reg.py
@@ -0,0 +1,80 @@
+import os
+from pathlib import Path
+from time import time
+from jax.random import PRNGKey, split
+
+from tensorflow_probability.substrates import jax as tfp
+tfb = tfp.bijectors
+
+import equinox as eqx
+import optax
+from tqdm import tqdm
+
+from src import GP_kernels as gpk
+from src.utils.common_training_functions import make_step, shard_data
+from src.utils.trace_dataset import load_traces, sample_random_batch
+from src.utils.setup_logger import setup_logger
+from src.utils.miscellaneous import dict_to_namedtuple
+
+import logging
+logger = setup_logger(__name__, level=logging.INFO)
+
+
+if __name__ == "__main__":
+  traces = load_traces("experiments/bayesian_reg/traces.pkl")
+  means_and_stds = load_traces("experiments/bayesian_reg/means_and_stds.pkl")
+  means_and_stds = dict_to_namedtuple(means_and_stds)
+  
+  c = gpk.GPInferenceCfg(
+    d_model = 128,
+    dropout_rate = 0.1,
+    discrete_mlp_width = 512,
+    discrete_mlp_depth=1,
+    continuous_flow_blocks=8,
+    continuous_flow_num_layers_per_block=2,
+    continuous_flow_num_augment=91,
+    num_enc_layers=5,
+    max_discrete_choices =6,
+    num_input_variables = (1,),
+    num_observations =6,
+    means_and_stds=means_and_stds,
+  )
+  inference = gpk.GPInference(key=PRNGKey(0),c=c)
+  
+  inference.log_p(traces[1], PRNGKey(0))
+  
+  num_steps = 10000
+  optim = optax.chain(
+      optax.clip_by_global_norm(5.0),
+      optax.adamw(
+          learning_rate=optax.cosine_onecycle_schedule(
+              num_steps,
+              0.0005,
+              0.01,
+              1e1,
+              1e2,
+          ),
+          weight_decay=0.0005,
+      ),
+  )
+  
+  batch_size = 1024
+  
+  opt_state = optim.init(eqx.filter(inference, eqx.is_inexact_array))
+  
+  key = PRNGKey(573)
+  out_path = Path("tmp/")
+  os.makedirs(out_path, exist_ok=True)
+  for i in tqdm(range(num_steps), desc="100k_blr_0005"):
+      start = time()
+      batch_traces = sample_random_batch(traces, batch_size)
+      l, inference, opt_state, key = make_step(inference, opt_state, key, batch_traces, batch_size, optim)
+      end = time()
+      if i % 100 == 0 or i == 1:
+        logger.info(f"{l.item()} t {end-start}")
+        # print("l", l, "t", end - start)
+        #save model to dummy file
+        p = out_path / f"100k_blr_0005.eqx"
+        eqx.tree_serialise_leaves(p, inference)
+  
+  
